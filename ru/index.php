f4b2bbcfb1af72ec143deda2eb9e6ac95f875415
olegsher <github@sher.biz>
2017-02-16 18:28:37 +0200

    first

--- ru/index.php
+++ ru/index.php
@@ -111,10 +111,10 @@ header('Location: http://www.rentcarisrael.online/thankyou.php');
 <?php include("_teaser.html"); ?>
 <?php include("_services.html"); ?>
 
-
+<?php include("_vehicles.html"); ?>
 <?php include("_newsletter.html"); ?>
 <?php include("_bottom.html"); ?>
-<?php include("_vehicles.html"); ?>
+
 
 <?php include("_footer.html"); ?>
 <?php include("_checkout.html"); ?>
